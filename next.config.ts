86ec823411bf3de74fd4fe3987e562ad71965cf3
param-9204 <param.t@shaligraminfotech.com>
2026-08-19 15:33:10 +0530

    chore: add tesseract.js dependencies to outputFileTracingIncludes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@ffmpeg-installer/ffmpeg', 'fluent-ffmpeg', 'tesseract.js', 'tesseract.js-core'],
+  outputFileTracingIncludes: {
+    '/api/**/*': [
+      './node_modules/tesseract.js/**/*',
+      './node_modules/tesseract.js-core/**/*',
+    ],
+  },
 };
 
 export default nextConfig;
